7631ac722e662052c9bb632c85c6e4751f949b8f
subrotomojumder <97802263+subrotomojumder@users.noreply.github.com>
2022-09-29 13:42:58 +0600

    get local storage data problem solve

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -5,11 +5,11 @@ import { getStorageData, setStorageData } from '../FakeDB/FakeDB';
 import './Cart.css';
 
 const Cart = ({selectedVehicle}) => {
-    const [time, setTime] = useState()
+    const [time, setTime] = useState(' ')
    useEffect( ()=>{
-    const previewStorage = getStorageData();
-    setTime(previewStorage)
-   }, [])
+        const previewStorage = getStorageData();
+        setTime(previewStorage)
+    }, [])
     const totalPrice = selectedVehicle.reduce((previewSum, current) => previewSum + current.ticketPrice, 0);
     const handleBreakTime = (minute) => {
         setTime(minute)
@@ -23,16 +23,16 @@ const Cart = ({selectedVehicle}) => {
                 <div className='mt-4'>
                     <h2 className='text-lg font-semibold'>Add a Station Break</h2>
                     <div className='grid grid-cols-4 shadow-lg py-[4px] bg-zinc-200 rounded-lg my-2 md:pl-2'>
-                        <div onClick={()=>handleBreakTime(10)} className='btn btn-warning font-medium rounded-full bg-white w-[46px]'>
+                        <div onClick={()=>handleBreakTime('10minute')} className='btn btn-warning font-medium rounded-full bg-white w-[46px]'>
                             <p>10<span className='text-sm lowercase'>m</span></p>
                         </div>
-                        <div onClick={()=>handleBreakTime(15)} className='btn btn-warning font-medium rounded-full bg-white w-[46px]'>
+                        <div onClick={()=>handleBreakTime('15minute')} className='btn btn-warning font-medium rounded-full bg-white w-[46px]'>
                             <p>15<span className='text-sm lowercase'>m</span></p>
                         </div>
-                        <div onClick={()=>handleBreakTime(20)} className='btn btn-warning font-medium rounded-full bg-white w-[46px]'>
+                        <div onClick={()=>handleBreakTime('20minute')} className='btn btn-warning font-medium rounded-full bg-white w-[46px]'>
                             <p>20<span className='text-sm lowercase'>m</span></p>
                         </div>
-                        <div onClick={()=>handleBreakTime(30)} className='btn btn-warning font-medium rounded-full bg-white w-[46px]'>
+                        <div onClick={()=>handleBreakTime('30minute')} className='btn btn-warning font-medium rounded-full bg-white w-[46px]'>
                             <p>30<span className='text-sm lowercase'>m</span></p>
                         </div>
                     </div>
@@ -40,7 +40,7 @@ const Cart = ({selectedVehicle}) => {
                 <div className='border rounded-lg mt-4 p-1'>
                     <h3 className='text-orange-400 font-semibold'>Break & Expense Details</h3>
                     <h5 className='bg-zinc-200 p-2 font-bold rounded-md mt-2'>Total Expense: {totalPrice}.0<span className='text-xs'>Tk</span></h5>
-                    <h5 className='bg-zinc-200 p-2 font-bold rounded-md mt-2'>Break Time: {time}<span className='text-xs'>minute</span></h5>
+                    <h5 className='bg-zinc-200 p-2 font-bold rounded-md mt-2'>Break Time: {time}</h5>
                 </div>
             </div>
         </div>
